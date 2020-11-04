3856c6db6b2a37505940ec4fa3afdb12152d6d5b
timothee.denizou <timothee.denizou@epita.fr>
2020-11-04 15:43:11 +0100

    backpropagation working

--- src/Main.c
+++ src/Main.c
@@ -2,76 +2,147 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-//#include "NeuralNetwork/Brain.h"
-//#include "NeuralNetwork/Neuron.h"
-#include "NeuralNetwork/Network.h"
-#include "NeuralNetwork/Debug.h"
-#include "NeuralNetwork//Training.h"
-#include "NeuralNetwork/write_read_brain.h"
-//#include "PictureUtils/bmp24.h"
+
+#include "Network.h"
+#include "Training.h"
+#include "Debug.h"
+#include "write_read_brain.h"
 
 int main()
 {
-    Network net = demoWriteRead(5,3,1,5);
-    double input[5] = {1.0,0.2,0.6,0.0,0.8};
-    double desiredOutput[3] = {0.0,0.0,1.0};
-    demoTraining(&net, input, desiredOutput);
+    
+    Network net = newNetwork(2,2,1,1);
+    initNetwork(&net);
+    printNetwork(&net);
+    writeNetwork(&net);
+    
+    /*
+    Network net = readNetwork("network.txt");
+    printNetwork(&net);
+    */
 
-#if 0
-    // TRAINING FOR XOR
+    /*
+    checkForwardPropagation(&net, input, target);
+    printLayerValues(net.layers[0]);
+    printLayerValues(net.layers[1]);
+    printLayerValues(net.layers[2]);
 
-    for(int i = 0; i < nb_trainings;i++) // one training
-    {
-        printf("Training : %d\n",i);
-        for(int j = 0; j < 4; j++) // one batch of 4
+    printf("\n");
+    backPropagation(&net);
+
+    printLayerDerivatives(net.layers[1]);
+    printLayerDerivatives(net.layers[2]);
+    */
+    double data[2] = {0.0, 0.0};
+    double target[1] = {0.0};
+
+   for(int i = 0; i < 1000000; i ++)
+   {
+        switch(i%4)
         {
-            int choice ={j%4}; //rand()%4
-            double label[1] = {0};
-            double data[2] = {0.0,0.0};
-            switch (choice)
-            {
-                case 0:
-                    break;
-                case 1:
-                    data[0] = 0.0;
-                    data[1] = 1.0;
-                    label[0] = 1.0;
-                    break;
-                case 2:
-                    data[0] = 1.0;
-                    data[1] = 0.0;
-                    label[0] = 1.0;
-                    break;
-                case 3:
-                    data[0] = 1.0;
-                    data[1] = 1.0;
-                    break;
-                default:
-                    printf("choice random is not between 0 and 4]");
-                    printf("%d",choice);
-            }
-            printf("Data In :\n");
-            print_vector(2,data);
-            printf("Forward prop start :\n");
-            forwardPropagation(&net, data);
-            double cost = meanSquareFunction(label, &net.layers[net.nbLayers-1]);
-            printf("Total cost : %lf\n", cost);
-            printf("Backprop start :\n");
-            backPropagation(&net);
-            printf("### backprop end ###\n\n");
+            case 0:
+                target[0] = 0;
+            break;
+            case 1:
+                data[0] = 0.0;
+                data[1] = 1.0;
+                target[0] = 1.0;
+            break;
+            case 2:
+                data[0] = 1.0;
+                data[1] = 0.0;
+                target[0] = 1.0;
+            break;
+            case 3:
+                data[0] = 1.0;
+                data[1] = 1.0;
+                target[0] = 0;
+            break;
         }
-        gradientDescent(&net,0.2);
-        printf("#### train end ####\n");
-        //double endData[1] = {0};
 
-        //printf("Data Out :\n");
-        //print_vector(1, endData);
-        //printf("Ground Truth :\n");
-        //print_vector(1,label);
+        forwardPropagation(&net, data, target);
+        backPropagation(&net);
+        gradientDescent(&net, 0.5);
+   }
 
+    for(int i = 0; i < 4; i ++)
+   {
+        switch(i%4)
+        {
+            case 0:
+                target[0] = 0;
+            break;
+            case 1:
+                data[0] = 0.0;
+                data[1] = 1.0;
+                target[0] = 1.0;
+            break;
+            case 2:
+                data[0] = 1.0;
+                data[1] = 0.0;
+                target[0] = 1.0;
+            break;
+            case 3:
+                data[0] = 1.0;
+                data[1] = 1.0;
+                target[0] = 0;
+            break;
+        }
+        checkForwardPropagation(&net, data, target);
+        printLayerValues(net.layers[2]);
+   }
 
+    printNetwork(&net);
+
+   /*
+    for (size_t i = 0; i < 10; i++)
+    {
+        double data[2] = {0.0, 0.0};
+        double target[1] = {0.0};
+        switch(i%4)
+        {
+            case 0:
+                target[0] = 0;
+            break;
+            case 1:
+                data[0] = 0.0;
+                data[1] = 1.0;
+                target[0] = 1.0;
+            break;
+            case 2:
+                data[0] = 1.0;
+                data[1] = 0.0;
+                target[0] = 1.0;
+            break;
+            case 3:
+                data[0] = 1.0;
+                data[1] = 1.0;
+                target[0] = 0;
+            break;
+        }
+
+        if((i < 10) || i > 1000000 - 10)
+        {
+            printf("Data In\n");
+            print_vector(2, data);
+            printf("Target\n");
+            print_vector(1, target);
+        }
+        
+        forwardPropagation(&net, data, target);
+        backPropagation(&net);
+        gradientDescent(&net, 12);
+
+        if((i < 10) || i > 1000000 - 10)
+        {   
+            double lastvalue = net.layers[2].neurons[0].value;
+            printf("last layer neuron %d value %lf\n",0,lastvalue);
+            printf("\n");
+            printNetwork(&net);
+        }
+        
     }
-#endif
-
+    */
+    
     return 0;
 }
--- src/NeuralNetwork/Training.h
+++ src/NeuralNetwork/Training.h
@@ -3,8 +3,8 @@
 #include "Network.h"
 
 double sigmoid(double n);
-void forwardPropagation(Network* net, double* data);
-double meanSquareFunction(double* target, Layer* layer);
+void forwardPropagation(Network* net, double* data, double* target);
+double meanSquareFunction(Layer* layer, double* target);
 void backPropagation(Network* net);
 void gradientDescent(Network* net, double learningRate);
 #endif
