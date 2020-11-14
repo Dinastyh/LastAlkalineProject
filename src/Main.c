0b8874b93fca08e493cecbc8df4e237b29c0fd9e
Paul LEGE <paul.lege@epita.fr>
2020-11-14 22:56:39 +0100

    Buttons + callback

--- src/Main.c
+++ src/Main.c
@@ -5,6 +5,10 @@
 #include <gtk/gtk.h>
 
 void onDestroy(GtkWidget *pWidget, gpointer pData);
+void onExecute(GtkWidget *pWidget, gpointer pData);
+void onSelect(GtkWidget *pWidget, gpointer pData);
+void onSave(GtkWidget *pWidget, gpointer pData);
+
 int main(int argc,char** argv)
 {
     gtk_init(&argc, &argv);
@@ -23,7 +27,11 @@ int main(int argc,char** argv)
     //Define buntons
     exeBtn = gtk_button_new_with_label("Execute")
     selectBtn = gtk_button_new_with_label("Select");
-    saveBtn = gtk_button_new_with_label("Save");
+    saveBtn = gtk_button_new_from_label("Save");
+    
+    g_signal_connect(G_OBJECT(exeBtn), "released", G_CALLBACK(onExecute), NULL);
+    g_signal_connect(G_OBJECT(selectBtn), "released", G_CALLBACK(onSelect), NULL);
+    g_signal_connect(G_OBJECT(saveBtn), "released", G_CALLBACK(onSelect), NULL);
 
     gtk_widget_show_all(window);
     gtk_main();
@@ -36,4 +44,12 @@ void onDestroy(GtkWidget *pWidget, gpointer pData)
     exit(EXIT_SUCCESS);
 }
 
-
+void onExecute(GtkWidget *pWidget, gpointer pData)
+{
+}
+void onSelect(GtkWidget *pWidget, gpointer pData)
+{
+}
+void onSave(GtkWidget *pWidget, gpointer pData)
+{
+}
