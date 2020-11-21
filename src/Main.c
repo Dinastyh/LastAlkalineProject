4bec37f768325151b2d46f1266aa3b1bacddd813
Paul LEGE <paul.lege@epita.fr>
2020-11-21 14:41:31 +0100

    Add path selection

--- src/Main.c
+++ src/Main.c
@@ -3,14 +3,28 @@
 #include <math.h>
 #include "Ui/Ui.h"
 #include <gtk/gtk.h>
-
+static gchar* file;
 void onDestroy(GtkWidget *pWidget, gpointer pData);
 void onExecute(GtkWidget *pWidget, gpointer pData);
 void onSelect(GtkWidget *pWidget, gpointer pData);
 void onSave(GtkWidget *pWidget, gpointer pData);
+void takeFolder(GtkWidget *button, GtkWidget* fileSelection);
+void setFile(gchar* path);
+gchar* getFile();
+
+void setFile(gchar* path)
+{
+    file = path;
+}
+
+gchar* getFile()
+{
+    return file;
+}
 
 int main(int argc,char** argv)
-{ 
+{
+    setFile(NULL);
     GtkWidget* window;
     GtkWidget* exeBtn;
     GtkWidget* selectBtn;
@@ -41,8 +55,8 @@ int main(int argc,char** argv)
     gtk_box_pack_start(GTK_BOX(toolsBarre), exeBtn, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(toolsBarre), saveBtn, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(displayCenter), toolsBarre, FALSE, FALSE, 0);
+
     //Add on Window
-    displayTxt(displayCenter, "hsefahflkjhrglkahjdgkjhdlfgjkhalkfhgldkjffffffffffffffffffaruhgpairhgiherihgalkrghladhrjhlkjhlajhglkjharjghjahrjgrjgjarjnejfwuenfjnwjefnnwenfljbrlkbalkrgjb");
     gtk_container_add(GTK_CONTAINER(window), displayCenter);
     gtk_widget_show_all(window);
     gtk_main();
@@ -61,7 +75,23 @@ void onExecute(GtkWidget *pWidget, gpointer pData)
 }
 void onSelect(GtkWidget *pWidget, gpointer pData)
 {
-    exit(EXIT_SUCCESS);
+    GtkWidget* fileSelection =  gtk_file_selection_new(g_locale_to_utf8("Select a file", -1, NULL, NULL, NULL));
+    gtk_widget_show(fileSelection);
+    gtk_window_set_modal(GTK_WINDOW(fileSelection), TRUE);
+    g_signal_connect(GTK_OBJECT(GTK_FILE_SELECTION(fileSelection)->ok_button), "released", G_CALLBACK(takeFolder), fileSelection);
+    g_signal_connect(GTK_OBJECT(GTK_FILE_SELECTION(fileSelection)->cancel_button), "released", G_CALLBACK(gtk_widget_destroy), fileSelection);
+}
+
+void takeFolder(GtkWidget* button, GtkWidget* fileSelection)
+{
+    const gchar* path;
+    GtkWidget* dialog;
+    path = gtk_file_selection_get_filename(GTK_FILE_SELECTION(fileSelection));
+    dialog = gtk_message_dialog_new(GTK_WINDOW(fileSelection), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, "You have slected ;\n%s", path);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+    gtk_widget_destroy(fileSelection);
+    setFile(path);
 }
 void onSave(GtkWidget *pWidget, gpointer pData)
 {
